f1c7ec50a161a390968905a603791b2f480a1667
frosty <passedgoandgot200@gmail.com>
2024-05-26 10:57:28 -0400

    new blocks etc

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER " "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 45
+#define MAX_BLOCK_OUTPUT_LENGTH 40
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 0
@@ -17,10 +17,10 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)             \
-    X("", "sb-memory", 5, 0) \
-    X("", "sb-volume", 0, 1)  \
-    X("", "sb-battery", 5, 0) \
-    X("", "sb-date", 60, 0)
+#define BLOCKS(X) \
+    X("", "sb-memory", 5,  12) \
+    X("", "sb-disk",   5,  11) \
+    X("", "sb-volume", 30, 1) \
+    X("", "sb-date",   60, 10)
 
 #endif  // CONFIG_H
